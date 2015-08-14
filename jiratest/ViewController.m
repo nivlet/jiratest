9baa33fde07ab02c6fa6028c2d7b8da0d8a256c9
Philip Waller <Philip.Waller@hcss.com>
2015-08-14 16:31:52 -0500

    HJ-47 Testing this fancy integration with JIRA

--- jiratest/ViewController.m
+++ jiratest/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // I'M ADDING COMMENTS SO THAT THIS FILE WILL BE EDITED OMGEEZEBURGER
 }
 
 - (void)didReceiveMemoryWarning {
